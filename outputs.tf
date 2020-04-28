001619addea54258a62f26cf0cefd1e1def5d536
peric <drazen.peric@kiwi.com>
2020-04-28 12:35:11 +0200

    Add port to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "elasticsearch_endpoint" {
-  value = "${var.cluster_name}-${var.node_group}.${var.namespace}.svc.cluster.local"
+  value = "${var.cluster_name}-${var.node_group}.${var.namespace}.svc.cluster.local:9200"
 }
