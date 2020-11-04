26b3d98b6fa84c1db119cfa155980c28a5034174
Milan Seman <milan.seman@kiwi.com>
2020-11-04 16:49:04 +0100

    feat(terraform): update providers

--- versions.tf
+++ versions.tf
@@ -1,3 +1,11 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    datadog = {
+      source = "terraform-providers/datadog"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
+  }
 }
