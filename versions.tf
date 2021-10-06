c075206e3b0f669fcadea29d2c8cae7c93b3d991
Róbert Selvek <robert.selvek@kiwi.com>
2021-10-06 16:21:01 +0200

    feat(snapshots): add automatic snapshot bucket creation

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     datadog = {
-      source = "DataDog/datadog"
+      source  = "DataDog/datadog"
       version = "~> 2.19.1"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
       version = "~> 1.3.2"
     }
   }
